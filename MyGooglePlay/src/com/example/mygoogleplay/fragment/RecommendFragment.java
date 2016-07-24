5376d4ca28b767784f9928e8cf28b5f0f7f2058e
董浩 <610389473@qq.com>
2016-07-24 21:40:34 +0800

    日志：推荐模块添加摇一摇切换效果和标签点击事件

--- MyGooglePlay/src/com/example/mygoogleplay/fragment/RecommendFragment.java
+++ MyGooglePlay/src/com/example/mygoogleplay/fragment/RecommendFragment.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import com.example.mygoogleplay.http.protocol.RecommendProtocol;
+import com.example.mygoogleplay.ui.fly.ShakeListener;
+import com.example.mygoogleplay.ui.fly.ShakeListener.OnShakeListener;
 import com.example.mygoogleplay.ui.fly.StellarMap;
 import com.example.mygoogleplay.ui.view.LoadingPager.ResultState;
 import com.example.mygoogleplay.utils.UIUtils;
@@ -12,7 +14,9 @@ import android.R.string;
 import android.graphics.Color;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RecommendFragment extends BaseFragment {
 
@@ -20,12 +24,19 @@ public class RecommendFragment extends BaseFragment {
 
 	@Override
 	public View OnCreateSuccessView() {
-		StellarMap stellarMap = new StellarMap(UIUtils.getContext());
+		final StellarMap stellarMap = new StellarMap(UIUtils.getContext());
 		stellarMap.setAdapter(new RecommendAdapter());
 		stellarMap.setRegularity(6, 9);// 设置9行6列的随机方式
 		int padding = UIUtils.dpi2px(10);
 		stellarMap.setInnerPadding(padding, padding, padding, padding);
 		stellarMap.setGroup(0, true);
+		ShakeListener listener = new ShakeListener(UIUtils.getContext());
+		listener.setOnShakeListener(new OnShakeListener() {
+			@Override
+			public void onShake() {
+				stellarMap.zoomIn();
+			}
+		});
 		return stellarMap;
 	}
 
@@ -55,7 +66,7 @@ public class RecommendFragment extends BaseFragment {
 		@Override
 		public View getView(int group, int position, View convertView) {
 			position += (group) * getCount(group - 1);
-			TextView textView = new TextView(UIUtils.getContext());
+			final TextView textView = new TextView(UIUtils.getContext());
 			textView.setText(data.get(position));
 			Random random = new Random();
 			int size = 16 + random.nextInt(10);
@@ -64,6 +75,13 @@ public class RecommendFragment extends BaseFragment {
 			int g = 30 + random.nextInt(200);
 			int b = 30 + random.nextInt(200);
 			textView.setTextColor(Color.rgb(r, g, b));
+			textView.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Toast.makeText(UIUtils.getContext(), textView.getText(),
+							Toast.LENGTH_SHORT).show();
+				}
+			});
 			return textView;
 		}
 
