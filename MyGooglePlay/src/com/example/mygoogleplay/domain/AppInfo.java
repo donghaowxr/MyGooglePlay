e0ace7a4e10dbecb43eec3d5086d9b7e90d7ac27
董浩 <610389473@qq.com>
2016-07-24 10:54:47 +0800

    日志：使用JSONObject的方式解析JSON数据

---
+++ MyGooglePlay/src/com/example/mygoogleplay/domain/AppInfo.java
@@ -0,0 +1,12 @@
+package com.example.mygoogleplay.domain;
+
+public class AppInfo {
+	public String des;
+	public String downloadUrl;
+	public String iconUrl;
+	public String id;
+	public String name;
+	public String packageName;
+	public long size;
+	public float stars;
+}
---
+++ MyGooglePlay/src/com/example/mygoogleplay/http/protocol/HomeProtocol.java
@@ -0,0 +1,55 @@
+package com.example.mygoogleplay.http.protocol;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.mygoogleplay.domain.AppInfo;
+
+public class HomeProtocol extends BaseProtocol<ArrayList<AppInfo>> {
+
+	@Override
+	public String getKey() {
+		return "home";
+	}
+
+	@Override
+	public String getParam() {
+		return "";// 由于需要字符串拼接，所以不能返回null
+	}
+
+	@Override
+	public ArrayList<AppInfo> parseData(String result) {
+		try {
+			JSONObject jo = new JSONObject(result);
+			JSONArray ja = jo.getJSONArray("list");
+			ArrayList<AppInfo> appinfoList = new ArrayList<AppInfo>();
+			for (int i = 0; i < ja.length(); i++) {
+				JSONObject joList = ja.getJSONObject(i);
+				AppInfo info = new AppInfo();
+				info.des = joList.getString("des");
+				info.downloadUrl = joList.getString("downloadUrl");
+				info.iconUrl = joList.getString("iconUrl");
+				info.id = joList.getString("id");
+				info.name = joList.getString("name");
+				info.packageName = joList.getString("packageName");
+				info.size = joList.getLong("size");
+				info.stars = (float) joList.getDouble("stars");
+				appinfoList.add(info);
+			}
+			JSONArray jaPic = jo.getJSONArray("picture");
+			ArrayList<String> pictures = new ArrayList<String>();
+			for (int i = 0; i < jaPic.length(); i++) {
+				String pic = jaPic.getString(i);
+				pictures.add(pic);
+			}
+			return appinfoList;
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
