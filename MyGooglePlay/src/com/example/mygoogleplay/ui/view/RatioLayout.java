468a386ec128afa129bb07fd5d667918c844f9ac
董浩 <610389473@qq.com>
2016-07-24 18:22:42 +0800

    日志：完成自定义按比例设置宽高控件

--- MyGooglePlay/src/com/example/mygoogleplay/ui/view/RatioLayout.java
+++ MyGooglePlay/src/com/example/mygoogleplay/ui/view/RatioLayout.java
@@ -6,18 +6,31 @@ import android.widget.FrameLayout;
 
 public class RatioLayout extends FrameLayout {
 	private final static String namespace="http://schemas.android.com/apk/res/com.example.mygoogleplay";
+	private float ratio;
 	public RatioLayout(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 	}
 
 	public RatioLayout(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		float ratio=attrs.getAttributeFloatValue(namespace, "ratio", -1);
-		System.out.println(ratio);
+		ratio = attrs.getAttributeFloatValue(namespace, "ratio", -1);
 	}
 
 	public RatioLayout(Context context) {
 		super(context);
 	}
-
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int widthMode= MeasureSpec.getMode(widthMeasureSpec);//获取控件宽度模式
+		int width=MeasureSpec.getSize(widthMeasureSpec);//获取控件宽度真实值
+		int heightMode=MeasureSpec.getMode(heightMeasureSpec);
+		int height=MeasureSpec.getSize(heightMeasureSpec);
+		if (widthMode==MeasureSpec.EXACTLY&&heightMode!=MeasureSpec.EXACTLY&&ratio>0) {
+			int imageWidth=width-getPaddingLeft()-getPaddingRight();
+			int imageHeight=(int) (imageWidth/ratio+0.5f);
+			height=imageHeight+getPaddingTop()+getPaddingBottom();
+		}
+		heightMeasureSpec=MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
 }
